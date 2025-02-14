bbd939e52a296830aef63331cc4fac68e868c2c0
greg <1304055639@qq.com>
2025-02-14 12:22:02 +0800

    Update EmailServiceImpl.java

--- src/main/java/org/code/airportitemstorage/serviceImpl/email/EmailServiceImpl.java
+++ src/main/java/org/code/airportitemstorage/serviceImpl/email/EmailServiceImpl.java
@@ -1,19 +1,18 @@
 package org.code.airportitemstorage.serviceImpl.email;
 
 import jakarta.annotation.Resource;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.code.airportitemstorage.service.email.IEmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements IEmailService {
-    private final JavaMailSender mailSender;
+    @Resource
+    private JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
     private String from;
